c9b71a73ceebbab28102eb1597085ce4e66029dd
TPriince <tobiloba110@gmail.com>
2022-12-20 03:06:33 +0000

    Task 1 - Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+  let task = false;
+  let task2 = true;
 
-    if (trueOrFalse) {
-        let task = true;
-        let task2 = false;
-    }
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
 
-    return [task, task2];
+  return [task, task2];
 }
