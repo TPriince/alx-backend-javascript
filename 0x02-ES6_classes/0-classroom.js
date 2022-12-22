f0855f87ad12f3b7929ee3cdde108025ac707979
TPriince <tobiloba110@gmail.com>
2022-12-22 20:46:53 +0000

    Task 0 - You used to attend a place like this at some point

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,9 @@
 export default class ClassRoom {
+    /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
     constructor(maxStudentsSize) {
         this._maxStudentsSize = maxStudentsSize;
     }
