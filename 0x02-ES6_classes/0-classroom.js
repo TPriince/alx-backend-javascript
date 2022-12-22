41f081bde151236d47ed5dc49df2327de11c8ec5
TPriince <tobiloba110@gmail.com>
2022-12-22 20:48:17 +0000

    Task 0 - You used to attend a place like this at some point

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,10 +1,13 @@
+/**
+ * Represents a class room.
+ */
 export default class ClassRoom {
-    /**
+  /**
    * Creates a new @see {@link ClassRoom}.
    * @param {Number} maxStudentsSize - The maximum number of
    * students in the class.
    */
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
