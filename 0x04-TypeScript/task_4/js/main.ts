280f0eb1eadbd825855d3582611db56cca58b1f6
TPriince <tobiloba110@gmail.com>
2023-01-12 05:04:34 +0000

    Task 10 - Update task_4/js/main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -2,8 +2,8 @@ export const cpp: Subjects.Cpp = new Subjects.Cpp();
 export const java: Subjects.Java = new Subjects.Java();
 export const react: Subjects.React = new Subjects.React();
 export const cTeacher: Subjects.Teacher = {
-  firstName: 'Dennis',
-  lastName: 'Ritchie',
+  firstName: 'Tom',
+  lastName: 'Cruise',
   experienceTeachingC: 10,
 };
 
